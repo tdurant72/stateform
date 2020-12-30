8efeb7fc1e3a92e153448cb9107707509ebcd432
Tyus Durant <contact@tyusdurant.com>
2020-12-30 09:40:58 -0500

    inputs added

--- src/views/Step1.vue
+++ src/views/Step1.vue
@@ -1,41 +1,40 @@
 <template>
-  <div>{{ this.businessNameV2 }}</div>
-  <div>{{ businessNameRef }}</div>
-  <div>{{ businessNameReactive }}</div>
-  <button @click="calNumbers">Calc</button>
+  <div class="pt-2 flex flex-col items-start">
+    <label class="text-sm text-gray-800">Name</label>
+    <input
+      type="text"
+      class="border-gray-800 border-2 pl-2 rounded-md h-10"
+      placeholder="Full name here"
+      v-model="state.name"
+    />
+  </div>
+  <div class="flex ">
+    <button @click="navPrev">Home</button>
+    <button @click="navNext">Step 2</button>
+  </div>
 </template>
 
 <script>
 import global from "../global";
-import { ref, reactive } from "vue";
+import router from "../router";
+// import { ref, reactive } from "vue";
 export default {
   setup() {
     const { state } = global;
-    const businessNameRef = ref("My consulting firm ref");
-    const businessNameReactive = reactive("My consulting firm refactive");
-    const numberRef = ref(2);
-    const numberReative = reactive(4);
-    //some other function
-    const calNumbers = () => {
-      console.log(
-        "numberRef",
-        numberRef.value * 2,
-        "numberReactive",
-        numberReative * 2
-      );
+    const navNext = () => {
+      router.push("./step2");
     };
+    const navPrev = () => {
+      router.push("./home");
+    };
+    //some other function
+
     return {
       state,
-      businessNameRef,
-      businessNameReactive,
-      numberRef,
-      numberReative,
-      calNumbers,
+      navNext,
+      navPrev,
     };
   },
-  data: () => ({
-    businessNameV2: "My consulting firm v2",
-  }),
 };
 </script>
 
--- src/views/Step2.vue
+++ src/views/Step2.vue
@@ -1,9 +1,26 @@
 <template>
   <div>step2</div>
+  <div class="flex ">
+    <button @click="navPrev">Home</button>
+    <button @click="navNext">Step 2</button>
+  </div>
 </template>
 
 <script>
-export default {};
+import router from "../router";
+import global from "../global";
+export default {
+  setup() {
+    const { state } = global;
+    const navNext = () => {
+      router.push("./step2");
+    };
+    const navPrev = () => {
+      router.push("./home");
+    };
+    return { state, navNext, navPrev };
+  },
+};
 </script>
 
 <style></style>
